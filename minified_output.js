979a4382dce47addff62440609d5c859e50a1b68
Mike C <130037774+HokuBoi@users.noreply.github.com>
2023-09-11 16:17:51 +0200

    Update minified_output.js

---
+++ minified_output.js
@@ -0,0 +1,3 @@
+This is usually just an empty file. Keep it open in your preferred IDE or text editor (like Sublime Text for example) and, when the minify_code.py script is running, it will automatically add a minified version of your code to this output file.
+It watches for file changes in .js .html and .css files in the same directory the script is running in, and recursively in all subdirectories.
+Just remove this text from this minified_output.js file before starting.  
